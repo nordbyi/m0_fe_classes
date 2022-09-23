435e405b29ebf5be25ac30e8db2cc1d2ec784662
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-23 14:15:24 -0600

    Initial commit

--- oop.js
+++ oop.js
@@ -2,8 +2,20 @@
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn {
+  constructor(name = 'Stardust', color = 'Silver') {
+    this.name = name,
+    this.color = color
+  }
 
+  say(string) {
+    console.log(`*~*${string}*~*`)
+  }
+}
 
+var destiny = new Unicorn('Destiny')
+console.log(destiny)
+destiny.say('Word to your monther!')
 
 // Write a class called Vampire
 // it should have a dynamic name property
@@ -11,6 +23,29 @@
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
 
+class Vampire {
+  constructor(name = 'Drac', pet = 'bat') {
+    this.name = name,
+    this.pet = pet,
+    this.thirsty = true
+  }
+
+  drink(){
+    this.thirsty = false
+  }
+
+  itsMorbinTime(){
+    this.name === 'Morbius' && console.log(`IT'S MORBIN' TIME!`)
+  }
+}
+
+var morbius = new Vampire('Morbius', 'gargoyle')
+console.log(morbius)
+
+morbius.drink()
+console.log(morbius)
+
+morbius.itsMorbinTime()
 
 
 //  Write a Dragon class
@@ -20,6 +55,33 @@
 //  it should have a isHungry property that is true by default
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
+
+class Dragon {
+  #fullLevel;
+
+  constructor(name = 'Dargon', rider = 'Beowulf', color = 'red') {
+    this.name = name,
+    this.rider = rider,
+    this.color = color,
+    this.isHungry = true
+    this.#fullLevel = 0
+  }
+
+  eat() {
+    this.#fullLevel >= 4 ? console.log('Dragon is too full to eat any more') : this.#fullLevel++
+  }
+}
+
+var treva = new Dragon('Treva', 'Lancelot', 'silver')
+
+console.log(treva)
+treva.eat()
+console.log(treva)
+treva.eat()
+treva.eat()
+treva.eat()
+treva.eat()
+treva.eat()
 
 
 
@@ -31,3 +93,32 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit {
+  constructor(name = 'Frodo', disposition = 'gentle', age = 0) {
+    this.name = name,
+    this.disposition = disposition,
+    this.age = age,
+    this.isAdult = this.age >= 33 ? true : false
+    this.isOld = this.age >= 101 ? true : false
+    this.hasRing = this.name === 'Frodo' ? true : false
+  }
+
+  celebrateBirthday() {
+    this.age++;
+    if (this.age >= 33) this.isAdult = true;
+    if (this.age >= 101) this.isOld = true;
+    console.log(`${this.name} celerates birthday #${this.age}`);
+  }
+}
+
+var bilbo = new Hobbit('Bilbo', 'gentle', 100)
+var frodo = new Hobbit('Frodo', 'adventerous', 32)
+console.log(bilbo)
+console.log(frodo)
+
+bilbo.celebrateBirthday()
+console.log(bilbo)
+
+frodo.celebrateBirthday()
+console.log(frodo)
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+It felt good.  I thought they were fun challenges
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+Pretty good.  Need to take more breaks still and dringk more water.
 - What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+New chair, second/ third monitor and dongle to connect them, prep food in advance and tell friends and family goodbye!
